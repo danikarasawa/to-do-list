549dc74754decfd5c53a0a5d6e2043c4bd666608
dkarasawa <daniela.karasawa@mercadolivre.com>
2021-12-22 10:46:02 -0300

    added entity

--- src/main/java/com/danikarasawa/todolist/model/Task.java
+++ src/main/java/com/danikarasawa/todolist/model/Task.java
@@ -1,5 +1,15 @@
 package com.danikarasawa.todolist.model;
 
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
 @Entity
 @Table(name = "tasks")
 @Setter
@@ -7,6 +17,24 @@ package com.danikarasawa.todolist.model;
 @ToString
 public class Task {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private Date deadline;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
 }
